5ac59025bfaca4784665d2a1daacfb6f8314b4a0
James Valleroy <jvalleroy@mailbox.org>
2016-05-16 22:23:54 -0400

    Speed up login page.

--- tests/test_common.py
+++ tests/test_common.py
@@ -30,14 +30,14 @@ def feature(name):
 @given("I'm a logged in user")
 def logged_in_user(browser):
     browser.visit(config['DEFAULT']['url'])
-    first_boot = browser.find_link_by_href('/plinth/firstboot/state1/')
-    if first_boot:
-        first_boot.first.click()
+    login = browser.find_by_value('Login')
+    if login:
+        browser.fill('username', config['DEFAULT']['username'])
+        browser.fill('password', config['DEFAULT']['password'])
+        login.click()
+    else:
+        browser.find_link_by_href('/plinth/firstboot/state1/').first.click()
         browser.fill('username', config['DEFAULT']['username'])
         browser.find_by_id('id_password1').fill(config['DEFAULT']['password'])
         browser.find_by_id('id_password2').fill(config['DEFAULT']['password'])
         browser.find_by_value('Box it up!').click()
-    else:
-        browser.fill('username', config['DEFAULT']['username'])
-        browser.fill('password', config['DEFAULT']['password'])
-        browser.find_by_value('Login').click()
